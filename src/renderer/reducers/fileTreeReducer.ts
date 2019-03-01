17c3a346c317c99128ccb8eee5e1a19a7b1a5285
xdk78 <xdk78888@gmail.com>
2019-03-01 22:02:31 +0100

    feat: simplify reducers

--- src/renderer/reducers/fileTreeReducer.ts
+++ src/renderer/reducers/fileTreeReducer.ts
@@ -16,19 +16,24 @@ export const fileTreeReducer: Reducer<FileTreeState> = (
   state = defaultState,
   action: ToggleVisibilityAction | OpenDirectoryAction
 ) => {
-  const newState = Object.assign({}, state)
   switch (action.type) {
     case TOGGLE_VISIBILITY:
-      newState.isVisible = Object.assign({}, newState.isVisible)
-      newState.isVisible[action.payload.filePath] = !newState.isVisible[action.payload.filePath]
-      break
+      return {
+        ...state,
+        isVisible: {
+          ...state.isVisible,
+          [action.payload.filePath]: !state.isVisible[action.payload.filePath]
+        }
+      }
     case OPEN_DIRECTORY:
-      newState.openedDirectories = Object.assign({}, newState.openedDirectories, {
-        [action.payload.filePath]: action.payload.files
-      })
-      break
+      return {
+        ...state,
+        openedDirectories: {
+          ...state.openedDirectories,
+          [action.payload.filePath]: action.payload.files
+        }
+      }
     default:
       return state
   }
-  return newState
 }
